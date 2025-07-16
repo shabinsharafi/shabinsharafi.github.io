5a79b1d64cf99668d5bb584a1bd293d708c55b24
shabinsharafi <shabinsharafi@gmail.com>
2025-07-16 18:57:00 +0400

    'dd'

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3615557036"
+    serviceWorkerVersion: "275765168"
   }
 });
